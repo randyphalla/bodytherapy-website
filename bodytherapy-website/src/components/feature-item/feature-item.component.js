81acb136621bf25efc2a49548678c506ce36ca18
Randy Phallla <randyphalla@gmail.com>
2018-04-01 15:27:48 -0400

    Add new components

---
+++ bodytherapy-website/src/components/feature-item/feature-item.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FeatureItemComponent extends Component {
+  render() {
+    return (
+      <div>
+        FeatureItemComponent
+      </div>
+    );
+  }
+}
+
+export default FeatureItemComponent;
